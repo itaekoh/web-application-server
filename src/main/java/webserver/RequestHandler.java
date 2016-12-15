8e1a9d41e880a68086fd6ff3cef73238dcefe4a4
오이택 <itoh@nate.com>
2016-12-15 22:14:00 +0900

    no4

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,9 +9,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -39,20 +44,42 @@ public class RequestHandler extends Thread {
         	
         	String[] tokens = line.split(" ");
         	log.debug("tokens : {} / {} / {}",tokens[0], tokens[1], tokens[2]);
+        	//String url = tokens[1];
+        	int index = tokens[1].indexOf("?");
+        	if(index > -1) {
+	        	String requestPath = tokens[1].substring(0, index);
+	        	String queryString = tokens[1].substring(index+1);
+	        	Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+	        	log.debug("params = {}", params);
+	        	User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+	        	log.debug("user = {}",user);
+        	}
         	
-        	String url = tokens[1];
-        	
-        	
-        	
-        	
+        	int contentLength = 0;
         	while (!"".equals(line)) {
         		line = br.readLine();
+        		
+        		if(line.startsWith("Content-Length")) {
+        			String[] values =  line.split(":");
+        			contentLength = Integer.parseInt(values[1].trim());
+        		}
         		log.debug("request line : {}",line);
         	}
         	
+        	String path = tokens[1];
+        	if (tokens[0].equals("POST")) {
+        		String queryString = IOUtils.readData(br, contentLength);
+        		log.debug("POST_QueryString = {}", queryString);
+	        	
+        		Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+	        	log.debug("params = {}", params);
+	        	User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+	        	log.debug("user = {}",user);
+	        	path = "/index.html";
+        	}
         	
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+            byte[] body = Files.readAllBytes(new File("./webapp"+path).toPath());
             //byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
